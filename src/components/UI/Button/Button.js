10a90da0218cdd34c7b623db1d863fdc2ba13b4e
Frimu Daniel <frimudaniel17@gmail.com>
2021-12-04 20:23:00 +0100

    80

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,40 +1,17 @@
-import styled from "styled-components";
+import React from "react";
 
-const Button = styled.button`
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+import styles from "./Button.module.css";
 
-  @media (min-width: 768px) {
-    width: auto;
-  }
+// It will create unique classes of all the styles for this component.
 
-  &:focus {
-    outline: none;
-  }
+// We use css modules to keep our css in the css files and the js in js files
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
-
-// styled.button`` => this is called attacked template literal
-// (line 4) button is an element which is returned by the method styled which is then stored in the Button constant
-
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
